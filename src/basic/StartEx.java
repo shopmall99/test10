31ce27a65dfdbdb60db3ffedb345ca3b6537b5cd
shopmall99 <serbible2021@gmail.com>
2023-10-28 15:35:45 +0900

    2차 push

--- src/basic/StartEx.java
+++ src/basic/StartEx.java
@@ -5,6 +5,9 @@ public class StartEx {
 	public static void main(String[] args) {
 		System.out.println("1차 push");
 		System.out.println("1차 push");
+		
+		System.out.println("1차 push");
+		System.out.println("1차 push");
 
 	}
 
